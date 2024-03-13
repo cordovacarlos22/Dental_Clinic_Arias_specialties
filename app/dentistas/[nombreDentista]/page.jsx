c9b85a7f253f639b94881ec9fba4d5e1a3899c36
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-13 10:00:55 -0700

    feat(routes): :sparkles: add dentistas route

---
+++ app/dentistas/[nombreDentista]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dentita = ({params}) => {
+  return (
+    <>
+    <h1>hola dentista {params.nombreDentista}</h1>
+    </>
+  )
+}
+
+export default Dentita;
---
+++ app/dentistas/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dentistas = () => {
+  return (
+    <div>Dentistas</div>
+  )
+}
+
+export default Dentistas
