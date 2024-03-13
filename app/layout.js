1c26de2f3403a65c1715058012061d76c3b4da92
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-12 19:32:44 -0700

    feat: :art: project ready to start develoment

--- app/layout.js
+++ app/layout.js
@@ -4,11 +4,7 @@ import "./globals.css";
 import { Metadata } from "next";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata =  {
-  title: {
-    default:"Clinica Arias - Especialidades Dentales",
-    template:"\$ Clinica Arias - Especialidades Dentales "
-
-  },
+  title:"Clinica Arias - Especialidades Dentales",
   description: "Atención odontológica integral para niños, adultos, adultos mayores y personas con capacidades especiales.",
   
 };
