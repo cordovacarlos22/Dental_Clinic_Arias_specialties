c9ec4628d864d825ef7ded7c07b09e7ae7448574
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-12 18:06:35 -0700

    feat: :mag: add opengraph-image.png

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata =  {
   title: {
     default:"Clinica Arias - Especialidades Dentales",
-    template:"\$ Clinica Arias - Especialidades Dentales ",
-    image:"./opengraph-image.png"
+    template:"\$ Clinica Arias - Especialidades Dentales "
 
   },
   description: "Atención odontológica integral para niños, adultos, adultos mayores y personas con capacidades especiales.",
