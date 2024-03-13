402b44112299857e40d6befaff0f9e51a46bfbed
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-13 14:36:09 -0700

    refactor(components): :construction: add agende btn

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,23 @@
+import Link from "next/link";
 
 
 const Navbar = () => {
   return (
-    <nav>
-      <h1>navbar</h1>
+    <nav className="bg-purple-400 h-40 flex items-center justify-between">
+      <section className="logo-container mx-4">
+        <Link href="/">
+    
+        </Link>
+      </section>
+      <section>section</section>
+      <section>section</section>
+      <section>
+        <button className="bg-[#0B4F6F] text-white rounded-md p-2 m-4 hover:bg-blue-300 ">
+          <Link href="./citas">
+            Agende Hoy!
+          </Link>
+        </button>
+      </section>
     </nav>
   )
 }
