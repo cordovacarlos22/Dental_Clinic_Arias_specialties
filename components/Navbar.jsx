5fda9c4141bdddb8f242e7e2b60cba254e805416
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-13 18:40:49 -0700

    feat(components): :construction: desktop menu is almost done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,24 +1,49 @@
+import Image from "next/image";
 import Link from "next/link";
-
-
+import logo from "../public/logo.svg";
+import cruzRoja from "../public/CRUZ ROJA.svg";
+import mapLogo from "../public/map logo.svg";
 const Navbar = () => {
   return (
-    <nav className="bg-purple-400 h-40 flex items-center justify-between">
-      <section className="logo-container mx-4">
-        <Link href="/">
-    
-        </Link>
-      </section>
-      <section>section</section>
-      <section>section</section>
-      <section>
-        <button className="bg-[#0B4F6F] text-white rounded-md p-2 m-4 hover:bg-blue-300 ">
-          <Link href="./citas">
-            Agende Hoy!
+    <>
+      <nav className="hidden  md:flex  h-40  items-center justify-around">
+        {/* logo section */}
+        <section className="logo-container ">
+          <Link href="/">
+            <Image src={logo} alt="clinica arias logo" />
+
           </Link>
-        </button>
-      </section>
-    </nav>
+        </section>
+        {/* emergencies section */}
+        <section className="flex gap-2  item-center">
+          <Image src={cruzRoja} alt="red cross"></Image>
+          <aside className="flex flex-col items-center font-extrabold ">
+            <p className="flex  text-brand-darker text-2xl">+503 7322-2299</p>
+            <p className="flex col text-brand-dark font-bold">DISPONIBLES PARA EMERGENCIA</p>
+          </aside>
+        </section>
+        {/* location section */}
+        <section className="flex gap-2  item-center">
+          <Image src={mapLogo} alt="map icon"></Image>
+          <aside className="flex flex-col items-center font-extrabold ">
+            <p className="flex  text-brand-darker text-lg"> Final 5a Calle Poniente #14 Colonia San Manuel,</p>
+            <p className="flex col text-brand-dark font-bold">Atiquizaya, El Salvador(frente a Unidad de Salud)</p>
+          </aside>
+        </section>
+        {/* appointment section */}
+        <section>
+          <button className="bg-brand-darker text-white rounded-md p-2  hover:bg-brand-dark ">
+            <Link href="./citas">
+              Agende Hoy!
+            </Link>
+          </button>
+        </section>
+      </nav>
+
+      <nav className="mobile bg-black flex md:hidden">
+        <h1>mobile</h1>
+      </nav>
+    </>
   )
 }
 
