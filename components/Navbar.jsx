a1294af03e7d630ee3bc1b3cb2616dab95ca8e6c
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-15 15:14:59 -0700

    style(components): :iphone: update to nav bar. links generate from array

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,30 +9,53 @@ import { usePathname } from "next/navigation";
 
 
 const Navbar = () => {
+
+  let navLinks = [
+    {
+      name: "Inicio",
+      path: "/",
+    },
+    {
+      name: "Servicios",
+      path: "/servicios",
+    },
+    {
+      name: "Nuestro Equipo",
+      path: "/dentistas",
+    },
+    {
+      name: "Quienes Somos",
+      path: "/acerca",
+    },
+    {
+      name: "Contacto",
+      path: "/contacto",
+    },
+
+  ]
   const path = usePathname();
   return (
     <>
-      <nav className="hidden md:flex  gap-1 h-40  items-center justify-around relative">
+      <nav className="hidden md:flex  gap-1 h-40  items-center  justify-between mx-8 m-auto relative">
         {/* logo section */}
-        <section className="logo-container ">
+        <section className="logo-container  flex justify-start ">
           <Link href="/">
-            <Image className="md:size-28 lg:size-64"  src={logo}  alt="clinica arias logo" />
-
+            <Image className="md:size-36 lg:size-52" src={logo} alt="clinica arias logo" />
           </Link>
         </section>
         {/* emergencies section */}
         <section className="flex gap-2  item-center">
-          <Image className="size-8" src={cruzRoja} alt="red cross"></Image>
+          <Image className=" md:size-8 lg:size-12" src={cruzRoja} alt="red cross"></Image>
           <aside className="flex flex-col items-center font-extrabold ">
-            <p className="flex  text-brand-darker md:text-xs">+503 7322-2299</p>
+            <p className="flex  text-brand-darker md:text-xs lg:text-lg">+503 7322-2299</p>
             <p className="flex col text-brand-dark md:text-xs font-bold ">DISPONIBLES PARA EMERGENCIA</p>
           </aside>
         </section>
         {/* location section */}
         <section className="flex gap-2  item-center">
-          <Image  src={mapLogo} alt="map icon"></Image>
+          <Image src={mapLogo} alt="map icon"></Image>
           <aside className="flex flex-col items-center font-extrabold ">
-            <p className="flex  text-brand-darker md:text-xs"> Final 5a Calle Poniente #14 Colonia San Manuel,</p>
+            <p className="flex  text-brand-darker md:text-xs lg:text-lg"> Final 5a Calle Poniente #14 Colonia San Manuel,</p>
             <p className="flex col text-brand-dark md:text-xs lg:text">Atiquizaya, El Salvador(frente a Unidad de Salud)</p>
           </aside>
         </section>
@@ -44,31 +67,19 @@ const Navbar = () => {
             </Link>
           </button>
         </section>
-        <ul className="flex gap-5 absolute bottom-2 text-lg">
-          <li>
-            {path === "/" ?
-              (
-                ''
-              ) :
-              (
-                <Link href="/">Inicio</Link>
-              )}
-          </li>
-          <li>
-            <Link href="/servicios">Servicios</Link>
-          </li>
-          <li>
-            <Link href="/dentistas">Nuestro Equipo</Link>
-          </li>
-          <li>
-            <Link href="/acerca">Quienes Somos</Link>
-          </li>
-          <li>
-            <Link href="/acerca">Contactenos</Link>
-          </li>
-        </ul>
+        <section className="flex gap-5 absolute bottom-2 text-lg right-28">
+          {navLinks.map((e) => (
+            <ul className="" key={e.name}>
+              <li className="hover:bg-brand-light px-2 hover:rounded-md">
+                <Link href={e.path}>
+                  {e.name}
+                </Link>
+              </li>
+            </ul>
+          ))}
+        </section>
       </nav>
-      
+
       <nav className="mobile bg-gray-200 flex md:hidden">
         <h1>mobile</h1>
       </nav>
