28139253feea438fdc6b2f7e3d7f1394ff3f5df5
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-17 16:17:15 -0700

    feat(api): :sparkles: add dinamic route ID

---
+++ app/api/(auth)/dentistas/[id]/route.js
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server"
+
+export function GET(_, response) {
+  console.log('user id #',response.params.id)
+  const { id } = response.params
+
+  return NextResponse.json({ id })
+}
