709a6cd070021cc247109c6d26e01b1d0315f18c
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-17 16:14:47 -0700

    feat(routes): :zap: add dimimic route for dentistas

--- app/dentistas/[nombreDentista]/page.jsx
+++ app/dentistas/[nombreDentista]/page.jsx
@@ -1,5 +1,4 @@
 
-
 const Dentista = ({params}) => {
   return (
     <>
--- app/dentistas/page.jsx
+++ app/dentistas/page.jsx
@@ -1,9 +1,41 @@
+import Link from "next/link";
+
+import logo from '@/public/logo.svg'
+import Image from "next/image";
+
+const fecthDentistas = async () => {
+  let URL = process.env.URL //  GLOBAL URL
+  let dentistas = await fetch(`${URL}/api/dentistas`);
+  dentistas = await dentistas.json();
+  console.log(
+    dentistas, "Dentistas"
+  )
+  return dentistas
+}
 
 
-const Dentistas = () => {
+
+
+const Dentistas = async () => {
+
+  let dentistas = await fecthDentistas();
   return (
-    <div>Dentistas</div>
+    <div>
+      {dentistas.map((d) => (
+        <>
+          <Link href={`/dentistas/${d.firstName}`}>
+            <Image src={logo} alt="logo"></Image>
+            
+              <div>
+                <p>{d.firstName}</p>
+                <p>{d.email}</p>
+              </div>
+              
+          </Link>
+        </>
+      ))}
+    </div>
   )
 }
 
-export default Dentistas
+export default Dentistas;
