00198e5c626996342d5805cb32eca333dd9c0d93
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-17 13:13:44 -0700

    fix(models): :bug: fixed POST method(add doctor)

---
+++ app/models/dentistas.model.js
@@ -0,0 +1,31 @@
+//? creates a new schema for dentistas
+
+// imports Schema,model,models from mongoose
+import { Schema,model,models } from "mongoose";
+
+// function to create new user Schema 
+
+const DentistaSchema = new Schema (
+  {
+    firstName: {type:String, required:true, unique:false},
+    middleName:{type:String, required:true,unique:false},
+    firstLastName:{type:String, required:true,unique:false},
+    secondLastName:{type:String, required:true,unique:false},
+    userName:{type:String,required:true,unique:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    avatar:{type:String},
+    admin:{type:Boolean,default:false}
+  },
+  {
+    timestamps:true
+  }
+  );
+
+// user models check if already exits if not it creates it  to then  export as Dentista
+const Dentista = models.Dentistas || model("Dentistas", DentistaSchema);
+
+export default Dentista;  // export  userSchema  as Dentista
+
+
+
