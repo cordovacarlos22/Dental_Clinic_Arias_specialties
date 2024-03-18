e937da8455e8677183eebd07722fa61699f72feb
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-18 07:43:22 -0700

    fix(routes): :bug: change arrow function to a reg  async function

--- app/dentistas/page.jsx
+++ app/dentistas/page.jsx
@@ -3,25 +3,27 @@ import Link from "next/link";
 import logo from '@/public/logo.svg'
 import Image from "next/image";
 
-const fecthDentistas = async () => {
-  let URL = process.env.URL //  GLOBAL URL
-  let dentistas = await fetch(`https://dental-clinic-arias-specialties.vercel.app/api/dentistas`);
-  dentistas = await dentistas.json();
-  console.log(
-    dentistas, "Dentistas"
-  )
-  return dentistas
-}
+
+
+ export async function fecthDentistas(){
+   let URL = process.env.URL //  GLOBAL URL
+   let dentistas = await fetch(`${URL}api/dentistas`);
+    dentistas = await dentistas.json();
+    console.log(dentistas);
+    return dentistas;
+ }
+
 
 
 
 
 const Dentistas = async () => {
 
-  let dentistas = await fecthDentistas();
+  let element = await fecthDentistas();
+
   return (
     <div>
-      {dentistas.map((d) => (
+      {element.map((d) => (
         <>
           <Link href={`/dentistas/${d.firstName}`}>
             <Image src={logo} alt="logo"></Image>
