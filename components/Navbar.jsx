1af9e359127abe718985d7b8af76612d0b3795bd
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-18 07:55:15 -0700

    refactor(components): :lipstick: change layout need responsivenest

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -36,9 +36,9 @@ const Navbar = () => {
   const path = usePathname();
   return (
     <>
-      <nav className="hidden md:flex    bg-yellow-200 h-40  items-center   justify-around   relative">
+      <header className=" flex    bg-yellow-200 h-40  items-center   justify-around   relative">
         {/* logo section */}
-        <section className="flex ml-4">
+        <section className=" logo-container flex ml-4">
           <Link className="flex" href="/">
             <Image className="size-16 " src={logo} alt="red cross"></Image>
             <aside className="flex flex-col  ml-2 justify-center font-extrabold ">
@@ -47,42 +47,49 @@ const Navbar = () => {
             </aside>
           </Link>
         </section>
-        {/* emergencies section */}
-        <section className="flex gap-2  item-center">
-          <Image className="md:w-6 lg:w-10" src={cruzRoja} alt="red cross"></Image>
-          <aside className="inline flex-col items-center font-extrabold ">
-            <p className="text-brand-darker md:text-xs lg:text-lg">+503 7322-2299</p>
-            <p className="text-brand-dark md:text-xs font-bold ">DISPONIBLES PARA EMERGENCIA</p>
-          </aside>
-        </section>
-        {/* location section */}
-        <section className="flex gap-2  item-center">
-          <Image src={mapLogo} alt="map icon"></Image>
-          <aside className="inline flex-col items-center font-extrabold ">
-            <p className=" text-brand-darker md:text-xs lg:text-lg"> Final 5a Calle Poniente #14 Colonia San Manuel,</p>
-            <p className="text-brand-dark md:text-xs lg:text">Atiquizaya, El Salvador(frente a Unidad de Salud)</p>
-          </aside>
-        </section>
-        {/* appointment section */}
-        <section className="mx-2">
-          <button className="bg-brand-darker text-white rounded-md p-2  md:text-sm md:w-16 lg:text-lg lg:w-32 hover:bg-brand-dark ">
-            <Link href="./citas">
-              Agende Hoy!
-            </Link>
-          </button>
-        </section>
-        <section className="flex gap-5 absolute bottom-2 lg:text-lg md:text-sm right-28">
-          {navLinks.map((e) => (
-            <ul className="" key={e.name}>
-              <li className="hover:bg-brand-light px-2 hover:rounded-md">
-                <Link href={e.path}>
-                  {e.name}
+        <section className="right-container flex flex-col gap-6">
+          <section className="right-top-container  justify-around flex">
+            {/* emergencies section */}
+            <section className=" flex gap-2  item-center">
+              <Image className="md:w-6 lg:w-10" src={cruzRoja} alt="red cross"></Image>
+              <aside className="inline flex-col items-center font-extrabold ">
+                <p className="text-brand-darker md:text-xs lg:text-lg">+503 7322-2299</p>
+                <p className="text-brand-dark md:text-xs font-bold ">DISPONIBLES PARA EMERGENCIA</p>
+              </aside>
+            </section>
+            {/* location section */}
+            <section className="flex gap-2  item-center">
+              <Image src={mapLogo} alt="map icon"></Image>
+              <aside className="inline flex-col items-center font-extrabold ">
+                <p className=" text-brand-darker md:text-xs lg:text-lg"> Final 5a Calle Poniente #14 Colonia San Manuel,</p>
+                <p className="text-brand-dark md:text-xs lg:text">Atiquizaya, El Salvador(frente a Unidad de Salud)</p>
+              </aside>
+            </section>
+            {/* appointment section */}
+            <section className="mx-2">
+              <button className="bg-brand-darker text-white rounded-md p-2  md:text-sm md:w-16 lg:text-lg lg:w-32 hover:bg-brand-dark ">
+                <Link href="./citas">
+                  Agende Hoy!
                 </Link>
-              </li>
-            </ul>
-          ))}
+              </button>
+            </section>
+          </section>
+          <section className="right-bottom0container">
+            <nav className="flex gap-5  lg:text-lg md:text-sm ">
+              {navLinks.map((e) => (
+                <ul className="" key={e.name}>
+                  <li className="hover:bg-brand-light px-2 hover:rounded-md">
+                    <Link href={e.path}>
+                      {e.name}
+                    </Link>
+                  </li>
+                </ul>
+              ))}
+            </nav>
+          </section>
+         
         </section>
-      </nav>
+      </header>
 
       <nav className="mobile bg-gray-200 flex md:hidden">
         <h1>mobile</h1>
