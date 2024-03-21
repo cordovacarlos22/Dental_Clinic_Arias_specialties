a53bb05206528dbfe31b63b35bdcb5e3085b86b0
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-21 07:10:18 -0700

    feat(home page): :art: add animation left to right

--- app/page.js
+++ app/page.js
@@ -1,10 +1,18 @@
 
+import Navbar from "@/components/Navbar"
+import AnimationLeftToRight from "@/components/AnimationLeftToRight"
 
 const Home = () => {
   return (
-    <main className="bg-red-400">
-      Home
-    </main>
+    <>
+      {/* <Navbar /> */}
+      <AnimationLeftToRight>
+        <main className="bg-red-400 w-screen h-full">
+          Home
+        </main>
+      </AnimationLeftToRight>
+    </>
+    
   )
 }
 
