1a25cceea7cadffe86bcfd6a0864bfdedba47998
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-21 07:08:17 -0700

    refactor(layout): :art: update on code  structure

--- app/layout.js
+++ app/layout.js
@@ -31,9 +31,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
 
-      <body>
+      <body className="w-screen h-screen">
         <Navbar />
-        {children}
+        <main>
+          {children}
+        </main>
         <SpeedInsights />
       </body>
     </html>
