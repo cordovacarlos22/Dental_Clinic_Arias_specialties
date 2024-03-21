40509fc157d1c7d8f76b3b166654fa4035d878bb
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-21 12:40:48 -0700

    refactor(home page): :art: remo import for nav bar

--- app/page.js
+++ app/page.js
@@ -1,16 +1,12 @@
-
-import Navbar from "@/components/Navbar"
-import AnimationLeftToRight from "@/components/AnimationLeftToRight"
-
 const Home = () => {
   return (
     <>
-      {/* <Navbar /> */}
-      <AnimationLeftToRight>
+   
+     
         <main className="bg-red-400 w-screen h-full">
           Home
         </main>
-      </AnimationLeftToRight>
+     
     </>
     
   )
