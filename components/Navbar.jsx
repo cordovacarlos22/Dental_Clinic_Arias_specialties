f0a466dd8ceba1986797ae0c41550c5308f09e6f
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-22 19:54:27 -0700

    feat(components): :iphone: responsive is almost done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -53,9 +53,9 @@ const Navbar = () => {
       <AnimationRightToLeft>
         {/* The code you provided is defining the desktop version of the navigation bar for a website
       using React. Let's break down the structure and functionality of this code snippet: */ }
-        <header className=" hidden md:flex  md:flex-1 h-40  items-center   justify-around ">
+        <header className=" hidden   md:block  md:m-2  md:flex-1  lg:flex min-h-40  items-center   lg:justify-around ">
           {/* logo section */}
-          <section className=" logo-container flex ml-4">
+          <section className=" logo-container flex justify-start  lg:ml-4 md:m-2">
             <Link className="flex" href="/">
               <Image priority={false} className="size-18 " src={logo} alt="red cross"></Image>
               {/* <aside className="flex flex-col  ml-2 justify-center font-extrabold ">
@@ -64,29 +64,29 @@ const Navbar = () => {
             </aside> */}
             </Link>
           </section>
-          <section className="right-container flex flex-col gap-6">
+          <section className="right-container flex justify-center items-center flex-col gap-4">
             <section className="right-top-container flex gap-4  ">
               {/* emergencies section */}
               <section className=" flex  flex-col  item-center">
 
-                <p className="text-brand-darker  md:text-xs lg:text-2xl">503 7322-2299</p>
+                <p className="text-brand-darker  md:text-lg lg:text-2xl  font-bold">503 7322-2299</p>
 
-                <p className="text-brand-dark md:text-xs font-bold  gap-2  inline-flex">  <Image className="size-4" src={cruzRoja} alt="red cross"></Image>DISPONIBLES PARA EMERGENCIA</p>
+                <p className="text-brand-dark md:text-sm font-medium  gap-2  inline-flex">  <Image className="size-4" src={cruzRoja} alt="red cross"></Image>DISPONIBLES PARA EMERGENCIA</p>
 
               </section>
               {/* location section */}
               <section className="flex gap-2  item-center">
                 <Image priority={false} className="size-6" src={mapLogo} alt="map icon"></Image>
                 <aside className="inline flex-col items-center font-extrabold ">
-                  <p className=" text-brand-darker md:text-xs lg:text-lg"> Final 5a Calle Poniente #14 Colonia San Manuel,</p>
-                  <p className="text-brand-dark md:text-xs lg:text">Atiquizaya, El Salvador(frente a Unidad de Salud)</p>
+                  <p className=" text-brand-darker md:text-md lg:text-lg font-bold"> Final 5a Calle Poniente #14 Colonia San Manuel,</p>
+                  <p className="text-brand-dark md:text-sm ">Atiquizaya, El Salvador(frente a Unidad de Salud)</p>
                 </aside>
               </section>
               {/* appointment section */}
               <section className="mx-2">
-                <button className="bg-brand-darker text-white rounded-md p-2  md:text-sm md:w-16 lg:text-lg lg:w-36 hover:bg-brand-dark ">
+                <button className="bg-brand-darker text-white rounded-md p-2    lg:text-lg lg:w-36 hover:bg-brand-dark ">
                   <Link href="/citas">
-                    <span className="lg:text-xl md:text-sm">Agende Hoy!</span>
+                    <span className="lg:text-xl md:text- font-extrabold">Agende Hoy!</span>
                   </Link>
                 </button>
               </section>
@@ -97,7 +97,7 @@ const Navbar = () => {
               <nav className="flex gap-5  lg:text-lg md:text-sm ">
                 {navLinks.map((e) => (
                   <ul className="" key={e.name}>
-                    <li className="hover:text-brand-dark px-2 ">
+                    <li className="hover:text-brand-dark px-2 text-lg font-medium ">
                       <Link href={e.path}>
                         {e.name}
                       </Link>
@@ -121,12 +121,12 @@ const Navbar = () => {
           </Link>
           <section className="mobile-middle  flex flex-col  w-full  justify-between px-4">
             <aside className="flex justify-around">
-              <Image priority={true} src={logo} className="size-32 m-0 p-0" alt="logo"></Image>
+              <Image priority={true} src={logo} className="size-40 m-0 p-0" alt="logo"></Image>
 
               {/* is creating a button element that, when clicked, toggles the state
            of the `toggle` variable in the component. */ }
               <button onClick={() => setToggle(!toggle)} >
-                <Image priority={false} src={hamburgerMenu} alt="hamburger menu"></Image>
+                <Image priority={false} className="size-8 m-0 p-0" src={hamburgerMenu} alt="hamburger menu"></Image>
               </button>
             </aside>
 
@@ -141,8 +141,8 @@ const Navbar = () => {
               >
                 <nav className=" pb-4 ">
                   {navLinks.map((e) => (
-                    <ul className="w-28 " key={e.name}>
-                      <li className="hover:text-brand-dark px-2  ">
+                    <ul className="w-40 " key={e.name}>
+                      <li className="hover:text-brand-dark px-2  text-md m-2  font-medium ">
                         <Link onClick={() => setToggle(!toggle)} href={e.path}>
                           {e.name}
                         </Link>
@@ -158,17 +158,15 @@ const Navbar = () => {
           <section className="mobile-bottom flex w-full">
             <aside className="bg-gray-200 flex-1  flex flex-col  items-center p-2 border-r border-brand-dark">
               <Link href="tel:+503-7322-2299" className="flex flex-col justify-center items-center">
-                <Image className="" priority={false} src={phone} alt="phone" ></Image>
-                <p className="text-xs text-brand-dark">Llamar Ahora</p>
+                <Image className=" size-8 " priority={false} src={phone} alt="phone" ></Image>
+                <p className="text-sm text-brand-dark ">Llamar Ahora</p>
               </Link>
-
-
             </aside>
             <aside className="bg-gray-200 flex-1  ">
               <aside className="bg-gray-200 flex-1  flex flex-col  items-center p-2 ">
                 <Link className=" flex flex-col justify-center" href="https://maps.app.goo.gl/8ufftsE2aHmuYtHP7" target="_black" referrerPolicy="same-origen">
-                  <Image className="my-0 mx-auto" priority={false} src={location} alt="phone" ></Image>
-                  <p className="text-xs text-brand-dark">mapa</p>
+                  <Image className="my-0 mx-auto size-8" priority={false} src={location} alt="phone" ></Image>
+                  <p className="text-sm text-brand-dark">Mapa</p>
                 </Link>
 
               </aside>
@@ -176,9 +174,9 @@ const Navbar = () => {
             <aside className="bg-brand-dark flex-1 ">
               <aside className="bg-brand-darker flex-1  flex flex-col  items-center p-2 ">
                 <Link className=" flex flex-col justify-center" href='/citas'>
-                  <Image className="my-0 mx-auto" priority={false} src={calendar} alt="phone" ></Image>
+                  <Image className="my-0 mx-auto size-8" priority={false} src={calendar} alt="phone" ></Image>
 
-                  <p className="text-xs text-white">Agendar Cita</p>
+                  <p className="text-sm text-white">Agendar Cita</p>
                 </Link>
               </aside>
             </aside>
