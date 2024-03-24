7038f6ba9e2cb67e830e6a621f5191f0c925719a
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-24 12:20:53 -0700

    feat(home page): :sparkles: imported hero section

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero"
 
 
 const Main = () => {
   return (
     <div >
-      <main className=" mt-56 md:mt-36 bg-red-500 w-screen h-screen z-0  p-6   ">
-       <h1>HOME</h1>
+      <main className="   w-screen h-screen z-0">
+       <Hero/>
      </main>
     </div>
     
