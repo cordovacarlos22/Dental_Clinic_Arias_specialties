affbf97d1e95d9dfd0b683a6636cd3658499adb3
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-24 08:51:21 -0700

    feat(components): :sparkles: change  initial animation from x -100 to x -20

--- components/AnimationLeftToRight.jsx
+++ components/AnimationLeftToRight.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 const AnimationLeftToRight = ({ children}) => {
   return (
     <motion.div
-      initial={{ x: -100, opacity: 0 }}
+      initial={{ x: -20, opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
       transition={{ ease: 'easeInOut', duration: 1 }}
     >
