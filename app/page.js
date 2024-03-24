6bc0ae227b96cf39e7f524d889f287960c124f69
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-24 08:53:14 -0700

    feat(layout): :fire: remove import of home component and added mt-56 on mobile and mt-35 on media display

--- app/page.js
+++ app/page.js
@@ -1,15 +1,12 @@
-import Home from "@/components/Home"
+
 
 const Main = () => {
   return (
-    <>
-   
-     
-      <main className="bg-red-400 w-screen h-[calc(100vh-201px)]  lg:h-[calc(100vh-160px)] ">
-          <Home/>
-        </main>
-     
-    </>
+    <div >
+      <main className=" mt-56 md:mt-36 bg-red-500 w-screen  h-[calc(100vh-201px)]  lg:h-[calc(100vh-160px)] z-0  p-6   ">
+       <h1>HOME</h1>
+     </main>
+    </div>
     
   )
 }
