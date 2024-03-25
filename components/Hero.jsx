2c2e31bc0a137b2033427d03b258184592bc0e74
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-25 08:34:52 -0700

    feat(components): :sparkles: hero section component done

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 import map from "@/public/map logo.svg";
 import Link from 'next/link';
 import smile_model from "@/public/smile_model.svg";
-import  consultorio from "@/public/consultorio_clinica.svg"; 
-import  clinicaLobby from "@/public/clinica_arias.svg"
+import consultorio from "@/public/consultorio_clinica.svg";
+import clinicaLobby from "@/public/clinica_arias.svg"
+import HeroComponents from './HeroComponents';
+import family from '@/public/family.svg';
+import heart from '@/public/heart.svg';
+import surgeonDoctor from '@/public/surgeon-doctor.svg';
+import waves from '@/public/wavesBG.svg';
+
 const Hero = () => {
   return (
     <>
@@ -51,17 +57,46 @@ const Hero = () => {
         {/* top section */}
         <section className='flex   gap-2 lg:flex-row flex-col lg:justify-center items-center  mx-2   lg:overflow-hidden '>
           <Image className='md:rounded-l-xl  rounded-xl ' src={clinicaLobby} alt='area de espera clinica arias'></Image>
-          <Image className=' md:rounded-r-xl  rounded-md '  src={consultorio} alt=' area de consultorio'></Image>
+          <Image className=' md:rounded-r-xl  rounded-md ' src={consultorio} alt=' area de consultorio'></Image>
         </section>
         {/* bottom section */}
         <section className='  text-center mx-auto w-4/5  lg:w-1/2 '>
-         
+
           <p className='capitalize text-gray-200 font-medium tracking-wider  leading-loose'>
             Clínica Arias es un centro de atención dental que ofrece una amplia gama de especialidades dentales para pacientes de todas las edades. Con un equipo altamente capacitado de odontólogos y personal de apoyo, Clínica Arias brinda servicios de alta calidad, desde limpiezas dentales y tratamientos de caries hasta ortodoncia y cirugía oral. La clínica cuenta con instalaciones modernas y equipadas con la última tecnología en equipos dentales, lo que garantiza un tratamiento preciso y eficiente. Además, el equipo de Clínica Arias se enfoca en brindar un trato personalizado y amable a cada paciente, con el objetivo de hacer que la visita al dentista sea lo más cómoda y agradable posible.
           </p>
         </section>
       </article>
+      <section
+        className=" items-center flex flex-col md:flex-row gap-4  md:justify-around   bg-brand-light p-2 "
+        
+        
+      >
+        <HeroComponents
+          className1="flex    w-1/2 flex-col justify-center  text-center items-center  "
+          img={family}
+          alt="family icon"
+          title=" ODONTOLOGÍA FAMILIAR"
+          paragraph="Ocúpese de todas las necesidades de su familia"
+        />
 
+        <HeroComponents
+          className1="flex  w-1/2 flex-col justify-center text-center items-center  "
+          img={heart}
+          alt="Heart icon"
+          title=" NUESTROS PACIENTES NOS QUIEREN"
+          paragraph="Vamos más allá por nuestros pacientes"
+        />
+
+        <HeroComponents
+          className1="flex  w-1/2 flex-col justify-center text-center items-center "
+          img={surgeonDoctor}
+          alt="Surgeon Doctor icon"
+          title=" ODONTOLOGÍA DE URGENCIA"
+          paragraph="Listo para ayudar a nuestros pacientes"
+        />
+
+      </section>
     </>
   )
 }
