829bf24d883511c6f15e33e274ccfd311654f249
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-26 11:54:44 -0700

    feat(components): :sparkles: add new component for direciones

---
+++ components/Direciones.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Direciones = () => {
+  return (
+    <div className='text-white font-bold p-4'>
+       <h1>direcciones para llegar a clinica</h1>
+    </div>
+  )
+}
+
+export default Direciones
