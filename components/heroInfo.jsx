d7f0a1f393464bfa35892bfef9d3aa2b24a9334f
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-26 11:57:43 -0700

    feat(components): :sparkles: add new component for each card info

--- components/heroInfo.jsx
+++ components/heroInfo.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const heroInfo = () => {
+const HeroInfo = (props) => {
   return (
-    <section>
-      
+    <section className='flex flex-col justify-center items-center text-center bg-white m-6 '>
+      <h1 className='text-brand-darker font-bold md:text-2xl lg:text-3xl'>{props.title}</h1>
+      <article className={props.article}>
+        <section className={props.section1}>{props.section1_innerText}</section>
+        <section className={props.section2}>{props.section2_innerText}</section>
+      </article>
     </section>
   )
 }
 
-export default heroInfo
+export default HeroInfo
