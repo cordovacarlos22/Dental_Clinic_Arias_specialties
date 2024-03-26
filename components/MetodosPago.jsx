46f6552e949962910e42d598cd8448279d7fe509
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-26 11:56:35 -0700

    feat(components): :sparkles: add new component for metodos pago

---
+++ components/MetodosPago.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const MetodosPago = () => {
+  return (
+    <div
+    className='text-white font-bold p-4'
+    >
+      <h1>
+        MetodosPago
+      </h1>
+    </div>
+  )
+}
