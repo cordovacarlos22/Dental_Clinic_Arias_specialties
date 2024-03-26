56209d8b423340bef2a78d3f973693c0b14cfaf3
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-26 09:13:45 -0700

    feat(components): :art: improve code

--- components/HeroServicios.jsx
+++ components/HeroServicios.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 const HeroServicio = (props) => {
   return (
     <div className='w-[150px] h-[150px] flex flex-col  items-center m-2 p-2'>
-        <Image src={props.icon} alt={props.iconAlt}></Image>
-      <h1 className={props.textcolors}>{props.Tittle}</h1>
+        <Image className={props.iconClass} src={props.icon} alt={props.iconAlt}></Image>
+      <h1 className={props.headerClass}>{props.Tittle}</h1>
     </div>
   )
 }
