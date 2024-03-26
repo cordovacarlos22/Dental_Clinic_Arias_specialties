b41d8bc18ed8532b91e6c959fc7e65e1b77b4b22
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-26 11:56:00 -0700

    feat(components): :sparkles: add new component for horario

---
+++ components/Horario.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const Horario = () => {
+
+  let horarioDeAtencion = [
+    {
+      dia: 'Lunes',
+      horario:' 8 AM - 12 PM , 2 PM- 5 PM'
+
+    },
+    {
+    dia: 'Martes',
+      horario: ' 8 AM - 12 PM , 2 PM- 5 PM'
+
+    },
+    {
+    dia: 'Miercoles',
+      horario: ' 8 AM - 12 PM , 2 PM- 5 PM'
+
+    },
+    {
+      dia: 'Jueves',
+      horario: ' 8 AM - 12 PM , 2 PM- 5 PM'
+
+    },
+    {
+      dia: 'Viernes',
+      horario: ' 8 AM - 12 PM , 2 PM- 5 PM'
+
+    },
+    {
+      dia: 'Sabado',
+      horario: ' 8 AM - 12 PM , 2 PM- 5 PM'
+
+    },
+
+    {
+      dia: 'Domingo',
+      horario: 'Cerrado'
+    }
+
+  ]
+  return (
+    <div className='text-white font-bold p-4 '>
+     
+      {horarioDeAtencion.map((d)=>(
+        <p
+         key={d.dia}
+        >{d.dia} {d.horario}</p>
+      ))}
+      <p>Ahora Estamos Abiertos</p>
+    </div>
+  )
+};
+
+export default Horario
