38cbbc283b560a428b53c010bc149112989e2b56
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-28 15:05:34 -0700

    feat(components): :sparkles: add paragraft

--- app/citas/page.jsx
+++ app/citas/page.jsx
@@ -1,11 +1,15 @@
 import Calendario from "@/components/Calendario"
-
-
+import Image from "next/image"
+import Link from "next/link"
+import cross from '@/public/cross.svg'
 const Citas = () => {
   return (
     <div className=" w-screen ">
-      <div  className="w-full  flex justify-center gap-4 items-center bg-brand-light ">
-      <h1 className=" m-4 capitalize text-brand-dark lg:text-xl font-bold pt-4  justify-center text-center">Vamos a Programar su cita!</h1>
+      <div  className="w-full flex flex-col justify-center  items-center bg-brand-light ">
+      <h1 className=" m-4 capitalize text-brand-darker lg:text-xl font-bold pt-4  justify-center text-center">Vamos a Programar su cita!</h1>
+        <p className="text-brand-dark flex gap-2">
+          <Image src={cross} alt="cross"></Image>
+          Para Emergencias , por favor  llamarnos al <Link className="font-bold underline" href="tel:+503-7322-2299">503 7322-2299</Link>.</p>
       {/* <Image className="flex" src={smile} alt="smile"></Image> */}
       </div>
       <Calendario/>
