3dae1d7957e1c6d7eb0156a515f1c8536a5d1c44
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-28 16:05:32 -0700

    fix(home page): :lipstick: FIX MARGIN TOP

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Hero from "@/components/Hero"
 const Main = () => {
   return (
     <div >
-      <main className="  mt-60  md:mt-64 lg:mt-40 w-screen h-screen z-0">
+      <main className="  mt-56  md:mt-62 lg:mt-40 w-screen h-screen z-0">
        <Hero/>
      </main>
     </div>
