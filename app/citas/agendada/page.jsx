e0573f8d33a09e81e00eed823b97aad5d7a54ead
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-28 12:28:52 -0700

    style(components): :construction: working on styles

--- app/citas/agendada/page.jsx
+++ app/citas/agendada/page.jsx
@@ -3,10 +3,9 @@ import React from 'react'
 const page = () => {
   return (
     <div
-    className='w-screenshot'
-    
+    className='w-screenshot flex justify-center align-center text-white  text-3xl   text-center'
     >
-      <h1>Su cita fue recibida con exito!!!</h1>
+      <h1 className='text-center'>Su cita fue recibida con exito!!!</h1>
     </div>
   )
 }
