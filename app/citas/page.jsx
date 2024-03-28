1b125e790b29b04443551d5d9d4502cc2963affe
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-28 14:33:36 -0700

    feat(routes): :sparkles: add header

--- app/citas/page.jsx
+++ app/citas/page.jsx
@@ -1,10 +1,14 @@
 import Calendario from "@/components/Calendario"
-
+import Image from "next/image"
+import smile from '@/public/smile.jpg';
 
 const Citas = () => {
   return (
     <div className=" w-screen ">
-      <h1>citas</h1>
+      <div  className="w-full  flex justify-center gap-4 items-center bg-brand-light ">
+      <h1 className=" m-4 capitalize text-brand-dark lg:text-xl font-bold pt-4  justify-center text-center">Vamos a Programar su cita!</h1>
+      {/* <Image className="flex" src={smile} alt="smile"></Image> */}
+      </div>
       <Calendario/>
     </div>
   )
