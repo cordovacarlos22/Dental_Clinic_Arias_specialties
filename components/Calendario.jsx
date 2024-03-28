2f19af638c8565046cb69ff2e537011a3292e773
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-28 14:52:21 -0700

    feat(components): :sparkles: add styles

--- components/Calendario.jsx
+++ components/Calendario.jsx
@@ -3,8 +3,17 @@ import React from 'react'
 import { InlineWidget } from "react-calendly";
 const Calendario = () => {
   return (
-    <div className='w-screen '>
-      <InlineWidget url="https://calendly.com/clinicaariasdenta/30min" />
+    <div className='w-screen h-screen overflow-visible '>
+      <InlineWidget
+     
+        pageSettings={{
+          backgroundColor: 'ffffff',
+          hideEventTypeDetails: false,
+          hideLandingPageDetails: false,
+          primaryColor: '0B4F6F',
+          textColor: '0170A3'
+        }}
+      url="https://calendly.com/clinicaariasdenta/30min" />
     </div>
   )
 }
