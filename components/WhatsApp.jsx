3ff9dfaf3e4b740b48c7f23c95571b3fddfafef7
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-28 15:22:58 -0700

    style(components): :lipstick: add w and h to whatsapp logo

--- components/WhatsApp.jsx
+++ components/WhatsApp.jsx
@@ -9,10 +9,10 @@ const WhatsApp = (props) => {
 
       <Link href='https://wa.me/50373222299' target='_blank'>
         <Image
+          className='w-10 md:w-12 lg:w-14 h-10 md:h-12 lg:h-14'
           src={whatsAppIcon}
           alt='whatsapp icon'
-          width={80}
-          height={80} />
+          />
       </Link>
     </>
   )
