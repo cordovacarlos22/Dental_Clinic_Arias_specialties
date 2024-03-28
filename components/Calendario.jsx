cecaf4045d875bc13e89a268f0cbf91651b795ae
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-28 15:19:12 -0700

    fix(components): :lipstick: fixed calendario styles

--- components/Calendario.jsx
+++ components/Calendario.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 import { InlineWidget } from "react-calendly";
 const Calendario = () => {
   return (
-    <div className='w-screen h-screen overflow-visible '>
+    <div className='w-screen h-screen  '>
       <InlineWidget
-     
+        styles={{
+          padding: '1rem', height: '100vh', width: '100vw', borderRadius: '25%'
+}}
         pageSettings={{
           backgroundColor: 'ffffff',
           hideEventTypeDetails: false,
