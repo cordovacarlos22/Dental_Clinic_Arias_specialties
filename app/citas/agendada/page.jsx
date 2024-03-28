1fa1d776700da36f63d62adf1631bc74acd58aee
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-28 12:26:21 -0700

    feat(components): :construction: add confirmacion cita

---
+++ app/citas/agendada/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div
+    className='w-screenshot'
+    
+    >
+      <h1>Su cita fue recibida con exito!!!</h1>
+    </div>
+  )
+}
+
+export default page
