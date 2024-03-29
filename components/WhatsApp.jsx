d8656c0a5fa5532012ac64e754b38efb6396e8d0
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-28 17:24:47 -0700

    style(components): :lipstick: maked icon bigger

--- components/WhatsApp.jsx
+++ components/WhatsApp.jsx
@@ -9,7 +9,7 @@ const WhatsApp = (props) => {
 
       <Link href='https://wa.me/50373222299' target='_blank'>
         <Image
-          className='w-10 md:w-12 lg:w-14 h-10 md:h-12 lg:h-14'
+          className='w-12 md:w-14 lg:w-16 h-12 md:h-14 lg:h-16'
           src={whatsAppIcon}
           alt='whatsapp icon'
           />
