80ea81ef0622d74d4eee04739b14e98df5d7532c
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-29 12:42:26 -0700

    feat(routes): :sparkles: style doctores page need to add images,nombre,especialidad

--- app/dentistas/page.jsx
+++ app/dentistas/page.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 import logo from '@/public/logo.svg'
 import Image from "next/image";
+import Footer from "@/components/Footer";
 
 
 
@@ -9,12 +10,9 @@ import Image from "next/image";
    let URL = process.env.URL //  GLOBAL URL
    let dentistas = await fetch(`${URL}api/dentistas`);
     dentistas = await dentistas.json();
-    console.log(dentistas);
+    // console.log(dentistas);
     return dentistas;
  }
-
-
-
 
 
 const Dentistas = async () => {
@@ -22,20 +20,31 @@ const Dentistas = async () => {
   let element = await fecthDentistas();
 
   return (
-    <div className="mt-56 md:mt-36 bg-red-500 w-screen  z-0  p-6 ">
-      {element.map((d) => (
-        <>
-          <Link href={`/dentistas/${d.firstName}`}>
-            <Image src={logo} alt="logo"></Image>
-            
+    <div className="  bg-white mt-56  md:mt-62 lg:mt-40 w-screen z-0 ">
+      <div className="w-full flex flex-col justify-center  items-center bg-brand-light ">
+        <h1 className="  m-4 capitalize text-brand-darker md:text-xl lg:text-3xl font-bold pt-4  justify-center text-center">Equipo dental de Clinica Arias</h1>
+      </div>
+         <div className="text-center  m-10">
+        <h2 >
+          En Clínica Arias, nuestro equipo de dentistas, especialistas, higienistas y personal de apoyo comparten un único objetivo: ayudar a nuestros pacientes a alcanzar sus objetivos de salud bucal. Entendemos que muchas personas descuidan acudir al dentista por inquietud, lo que puede provocar que los pequeños problemas se agranden con el tiempo. Es por eso que hacemos todo lo posible para ayudarlo a sentirse cómodo desde el momento en que cruza nuestras puertas. Queremos construir una relación duradera y de confianza con usted para poder guiarlo a lo largo de su viaje hacia una mejor salud bucal. Nuestros médicos se mantienen actualizados sobre su educación continua para que siempre podamos brindar los mejores tratamientos disponibles en el campo. No dude en conocernos un poco mejor explorando los perfiles de nuestros médicos a continuación en Clínica Arias.
+        </h2>
+         </div>
+      <section className="flex flex-1 flex-wrap  w-2/3 justify-center  items-center mx-auto">
+        {element.map((d) => (
+          <div className=" w-[220px] h-[220px] flex items-center justify-center mx-auto " key={d.firstName}>
+            <Link href={`/dentistas/${d.firstName}`}>
+              <Image src={logo} alt="logo"></Image>
               <div>
                 <p>{d.firstName}</p>
                 <p>{d.email}</p>
               </div>
-
-          </Link>
-        </>
-      ))}
+            </Link>
+          </div>
+        ))}
+        </section>
+        
+        <Footer/>
+      
     </div>
   )
 }
