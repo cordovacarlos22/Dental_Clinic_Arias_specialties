a3c9408ae96e990e583d98e946c0336aaa838edf
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-29 11:35:53 -0700

    feat(routes): :construction: WORKING ON CONTACTO FORM

--- app/contacto/page.jsx
+++ app/contacto/page.jsx
@@ -1,5 +1,41 @@
+'use client'
+
+
+import Footer from "@/components/Footer"
 import Link from "next/link"
+import { useState } from 'react'
+
+
+
 const Contacto = () => {
+  
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+  
+  const handleContactForm = async()=>{
+    const response = await fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        phone,
+        message
+      })
+    })
+    const data = await response.json()
+    console.log(data)
+    setName("")
+    setEmail("")
+    setPhone("")
+    setMessage("")
+  }
+
+
   return (
     <div className="mt-56  md:mt-62 lg:mt-40 w-screen h-screen z-0 ">
       <div className="w-full flex flex-col justify-center  items-center bg-brand-light ">
@@ -9,9 +45,10 @@ const Contacto = () => {
         {/* <Image className="flex" src={smile} alt="smile"></Image> */}
       </div>
 
-      <form className="bg-white">
-            
+      <form  className="bg-white">
+            <h2>aqui van a ir  los inputs </h2>
       </form>
+      <Footer/>
     </div>
   )
 }
