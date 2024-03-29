5bee2926b7a6ae00e03a37750def46e88c0429ad
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-29 16:30:04 -0700

    feat(components): :sparkles: add dirreciones how to get to clinic

--- components/Direciones.jsx
+++ components/Direciones.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Direciones = () => {
   return (
     <div className='text-white font-bold p-4 '>
-       <h1>direcciones para llegar a clinica</h1>
+       <h1 className='font-boldeder  capitalize text-xl'>direccione para llegar a clinica 📍</h1>
+      <p>Estamos ubicados en Atiquizaya al final de la 5a calle poniente frente a Unidad de Salud de Atiquizaya (FOSALUD), edificio de 2 pisos identificado con nuestro logo.</p>
     </div>
   )
 }
