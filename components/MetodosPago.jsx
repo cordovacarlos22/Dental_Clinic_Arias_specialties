b78d478f9abd5163a36e4ec25e05e1742dadd8cc
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-29 16:30:32 -0700

    feat(components): :sparkles: add payment methos

--- components/MetodosPago.jsx
+++ components/MetodosPago.jsx
@@ -5,9 +5,12 @@ export const MetodosPago = () => {
     <div
     className='text-white font-bold p-4'
     >
-      <h1>
-        MetodosPago
-      </h1>
+      <h1 className='text-xs lg:text-lg  font-extrabold  capitalize'> metodos de pago</h1>
+      <ol>
+        <li className='text-xs  md:text-md lg:text-lg m-2 p-2'> Efectivo 💵  </li>
+        <li className='text-xs  md:text-md lg:text-lg m-2 p-2'>Transferencia  de Fedecredito 🏦</li>
+        <li className='text-xs  md:text-md lg:text-lg m-2 p-2'>Transferencia de Banco Agricola 🏦</li>
+      </ol>
     </div>
   )
 }
