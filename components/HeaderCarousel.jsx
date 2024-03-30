97bb479fadb6956602aea82478a9b46a6e1037c2
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-30 09:39:57 -0700

    feat(components): :sparkles: ADD NEW DOCTORS

--- components/HeaderCarousel.jsx
+++ components/HeaderCarousel.jsx
@@ -5,12 +5,18 @@ import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
 
 import Image from 'next/image'
-import consultorio from "@/public/consultorio_clinica.svg";
+
 import clinica_1 from "@/public/clinica_1.svg";
 import clinica_2 from "@/public/clinica_2.svg";
 import clinica_3 from "@/public/clinica_3.svg";
 import clinica_4 from "@/public/clinica_4.svg";
 import clinica_5 from "@/public/clinica_5.svg";
+
+import clinica_7 from "@/public/clinica_7.svg";
+import clinica_8 from "@/public/clinica_8.svg";
+import clinica_9 from "@/public/clinica_9.svg";
+import clinica_10 from "@/public/clinica_10.svg";
+import clinica_11 from "@/public/clinica_11.svg";
 export default function HeroCarousel() {
 
   const clinicImages = [
@@ -24,22 +30,38 @@ export default function HeroCarousel() {
       alt: 'clinica arias',
     },
     {
-      url: consultorio,
-      alt: 'clinica arias ',
-    },
-
-    {
       url: clinica_5,
-      alt: 'clinica arias kinds',
+      alt: 'clinica arias',
     },
     {
       url: clinica_3,
-      alt: 'clinica arias kinds',
+      alt: 'clinica arias',
     },
     {
       url: clinica_4,
+      alt: 'clinica arias',
+    }, 
+    {
+      url: clinica_7,
       alt: 'clinica arias kinds',
     },
+    {
+      url: clinica_8,
+      alt: 'clinica arias',
+    },
+    {
+      url: clinica_9,
+      alt: 'clinica arias',
+    },
+    {
+      url: clinica_10,
+      alt: 'clinica arias',
+    },
+    {
+      url: clinica_11,
+      alt: 'clinica arias',
+    },
+
 
 
   ]
