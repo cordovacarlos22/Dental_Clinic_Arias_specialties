d639c10af4c403e7cb8e3e3e400543325ddfdfb2
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-30 08:20:11 -0700

    feat(routes): :sparkles: CONTACT  ROUTE DONE

--- app/contacto/page.jsx
+++ app/contacto/page.jsx
@@ -9,31 +9,9 @@ import { useState } from 'react'
 
 const Contacto = () => {
   
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [message, setMessage] = useState("");
   
-  const handleContactForm = async()=>{
-    const response = await fetch('/api/contact', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name,
-        email,
-        phone,
-        message
-      })
-    })
-    const data = await response.json()
-    console.log(data)
-    setName("")
-    setEmail("")
-    setPhone("")
-    setMessage("")
-  }
+  
+  
 
 
   return (
@@ -45,9 +23,20 @@ const Contacto = () => {
         {/* <Image className="flex" src={smile} alt="smile"></Image> */}
       </div>
 
-      <form  className="bg-white">
-            <h2>aqui van a ir  los inputs </h2>
-      </form>
+      <section className="">
+        <div className=" flex   p-2 flex-col justify-normal bg-brand-light  w-90 h-52  m-10 rounded-3xl text-brand-darker">
+          <h1 className="text-center  m-auto font-bold ">
+            para mayor informacion por favor llamarnos al <span className="underline">503 7322-2299</span>
+          </h1>
+          <span className="text-center  m-auto font-bold"> o bien puede usar nuestro chat de Whatsapp</span>
+          <div className="bg-green-600 hover:bg-green-500 w-60 text-center flex  rounded-xl p-2 justify-center items-center mx-auto mb-4  text-white">
+            <Link href='https://wa.me/50373222299' target='_blank'>
+              <button className="   ">Chat Whatsapp </button>
+            </Link>
+          </div>
+        </div>
+
+      </section>
       <Footer/>
     </div>
   )
